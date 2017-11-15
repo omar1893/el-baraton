18b85e0a5a9ab7355570b829e4f3a4b628618ecc
Omar Lopez <omarjlg1893@gmail.com>
2017-11-15 07:26:21 -0400

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Vuex from 'vuex'
+import Cart from '@/components/Cart'
+
+Vue.use(Router, Vuex)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Cart',
+      component: Cart
+    }
+  ]
+})
